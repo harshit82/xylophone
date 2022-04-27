d59699b2af354d7f10415f6f1701ec9f29ee665a
Harshit Tathagat <harshittathagat@gmail.com>
2022-04-27 11:18:07 +0530

    Xylophone App

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({Key? key}) : super(key: key);
+
+  void playSound(int noteNumber) {
+    final player = AudioCache();
+    player.play("note$noteNumber.wav");
+  }
+
+  Expanded buildKey({required Color color, required int note}) {
+    return Expanded(
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(primary: color),
+          onPressed: () {
+            playSound(note);
+          },
+          child: const Text("")),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.red, note: 1),
+              buildKey(color: Colors.orange, note: 2),
+              buildKey(color: Colors.yellow, note: 3),
+              buildKey(color: Colors.green, note: 4),
+              buildKey(color: Colors.teal, note: 5),
+              buildKey(color: Colors.blue, note: 6),
+              buildKey(color: Colors.purple, note: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
